b821b0877166ff16a7ac58e2548abca9d240fad0
Andreas Paech <me@andreas-paech.de>
2021-04-09 14:30:09 +0200

    final protokoll 09.04.

---
+++ HelloWorld/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    // main  Methode ist der EINSTIEGSPUNKT
+    public static void main(String[] args) {
+        // AUSGABE
+        System.out.println("Hello World!");
+    }
+}
